6d721ce66da1c5f8188a11c484412bd4719830b0
Jose Fernando Mendes da Costa <20161D13GR0155@LABS.GARANHUNS.IFPE>
2017-11-22 21:52:03 -0300

    corrigindo como a bola se movimenta

--- main.lua
+++ main.lua
@@ -6,16 +6,16 @@ cantos = {}
 local function desenharCantos()
 	local esquerda = display.newRect(0,0,10,display.contentHeight*2)
 	esquerda.anchorX=10
-	physics.addBody( esquerda, "static",{ density=3.0, friction=0.5, bounce=0.5 } )
+	physics.addBody( esquerda, "static" )
 	local cima = display.newRect(0,0,display.contentWidth*2,10)
 	cima:setFillColor(0)
-	physics.addBody( cima, "static",{ density=3.0, friction=0.5, bounce=0.5 } )
+	physics.addBody( cima, "static" )
 	local direita = display.newRect(display.contentWidth,0,10,display.contentHeight*2)
 	direita.anchorX=0
-	physics.addBody( direita, "static",{ density=3.0, friction=0.5, bounce=0.5 } )
+	physics.addBody( direita, "static" )
 	local baixo = display.newRect(0,display.contentHeight,display.contentWidth*2,10)
 	baixo:setFillColor(0)
-	physics.addBody( baixo, "static",{ density=3.0, friction=0.5, bounce=0.5 } )
+	physics.addBody( baixo, "static" )
 end
 
 local function arrastar(event)
@@ -47,7 +47,7 @@ local function desenharBlocos()
 			bloco.anchorX = 0
 			bloco.anchorY = 0
 			bloco:setFillColor(math.random(),math.random(),math.random())
-			physics.addBody( bloco, "static", { density=3.0, friction=0.5, bounce=0.5 } )
+			physics.addBody( bloco, "static" )
 			bloco:addEventListener( "collision", blocosListener )
 			table.insert(tabelas, bloco)
 			x = x + 64
@@ -56,19 +56,20 @@ local function desenharBlocos()
 	end
 end
 
-local function bolinhaListener(event)
+--[[local function bolinhaListener(event)
 	local bolinha = event.target
-	bolinha:applyForce(1,1,1,1)
-end
+	bolinha:setLinearVelocity( 20, 20 )
+end]]
 
 local plataforma = display.newRect(display.contentCenterX, display.contentHeight-60, 75, 15)
-physics.addBody( plataforma, "static", { density=3.0, friction=0.5, bounce=0.5 } )
+physics.addBody( plataforma, "static" )
 plataforma:addEventListener("touch", arrastar)
 
 bolinha = display.newCircle(display.contentCenterX,display.contentCenterY, 12)
-physics.addBody( bolinha, "dynamic" )
-bolinha:addEventListener("collision", bolinhaListener)
-bolinha:applyForce(0,1,0,0)
+physics.addBody( bolinha, "dynamic", {density=0, friction=0, bounce=1, radius=15} )
+bolinha:setLinearVelocity( 150, 150 )
+--bolinha:addEventListener("collision", bolinhaListener)
+--bolinha:applyForce(0,1,0,0)
 
 desenharBlocos()
 desenharCantos()
